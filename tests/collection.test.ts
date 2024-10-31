3e52c95db6f52b645d811a9975895935cdcbd12b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-10-31 17:51:55 +0200

    feat(fetch): add create function for collections on fetch - Add a generic create function for the fetch client to handle all types of entities - Make it work for collections - Add integration suite to test against the actual Typesense server

--- tests/collection.test.ts
+++ tests/collection.test.ts
@@ -2,11 +2,50 @@
 import type { InferNativeType } from "@/collection/base";
 
 import { collection } from "@/collection/base";
-import { describe, expectTypeOf, it } from "vitest";
+import { configure } from "@/config";
+import { create } from "@/http/fetch/create";
+import { downAll, upAll } from "docker-compose";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  expectTypeOf,
+  it,
+} from "vitest";
+
+const isCi = process.env.CI;
+
+beforeAll(async () => {
+  if (!isCi) {
+    await upAll({ cwd: __dirname, log: true });
+  }
+});
+
+afterEach(async () => {
+  await fetch("http://localhost:8108/collections/test", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "X-TYPESENSE-API-KEY": "xyz",
+    },
+  });
+});
+
+afterAll(async () => {
+  if (!isCi) {
+    await downAll({ cwd: __dirname, log: true });
+  }
+});
 
 describe("collection tests", () => {
-  it("can't have an optional default sorting field", () => {
-    collection({
+  const config = configure({
+    apiKey: "xyz",
+    nodes: [{ host: "localhost", port: 8108, protocol: "http" }],
+  });
+  it("can't have an optional default sorting field", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -19,9 +58,12 @@ describe("collection tests", () => {
       // @ts-expect-error This is erroring as expected
       default_sorting_field: "field",
     });
+    await expect(create(schema, config)).rejects.toThrow(
+      "Default sorting field `field` cannot be an optional field",
+    );
   });
-  it("can't have a string that's not sorted as a default sorting field", () => {
-    collection({
+  it("can't have a string that's not sorted as a default sorting field", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -32,9 +74,12 @@ describe("collection tests", () => {
       // @ts-expect-error This is erroring as expected
       default_sorting_field: "field",
     });
+    await expect(create(schema, config)).rejects.toThrow(
+      "Default sorting field `field` is not a sortable type",
+    );
   });
-  it("can't have a num field as a default sorting field", () => {
-    collection({
+  it("can't have a num field as a default sorting field", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -46,9 +91,12 @@ describe("collection tests", () => {
       // @ts-expect-error This is erroring as expected
       default_sorting_field: "field",
     });
+    await expect(create(schema, config)).rejects.toThrow(
+      "Default sorting field `field` is not a sortable type",
+    );
   });
-  it("can have a num field as a default sorting field", () => {
-    collection({
+  it("can have a num field as a default sorting field", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -58,9 +106,36 @@ describe("collection tests", () => {
       ],
       default_sorting_field: "field",
     });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "int32",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: true,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "field",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
+    });
   });
-  it("can have a string field that's sorted as a default sorting field", () => {
-    collection({
+  it("can have a string field that's sorted as a default sorting field", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -71,33 +146,113 @@ describe("collection tests", () => {
       ],
       default_sorting_field: "field",
     });
-  });
-  it("can't have a nested object field without nested fields enabled", () => {
-    // @ts-expect-error This is erroring as expected
-    collection({
-      name: "test",
-      fields: [
-        {
-          type: "object",
-          name: "field",
-        },
-      ],
-    });
+    const result = await create(schema, config);
 
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "string",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: true,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "field",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
+    });
+  });
+  it("can't have a nested object field without nested fields enabled", async () => {
+    await expect(
+      create(
+        // @ts-expect-error This is erroring as expected
+        collection({
+          name: "test",
+          fields: [
+            {
+              type: "object",
+              name: "field",
+            },
+          ],
+        }),
+        config,
+      ),
+    ).rejects.toThrow(
+      "Type `object` or `object[]` can be used only when nested fields are enabled by setting` enable_nested_fields` to true.",
+    );
+
+    await expect(
+      create(
+        collection({
+          name: "test",
+          fields: [
+            {
+              type: "object",
+              name: "field",
+            },
+          ],
+          // @ts-expect-error This is erroring as expected
+          enable_nested_fields: false,
+        }),
+        config,
+      ),
+    ).rejects.toThrow(
+      "Type `object` or `object[]` can be used only when nested fields are enabled by setting` enable_nested_fields` to true.",
+    );
+  });
+  it("can't have num_dim, vec_dist or hnsw_params on a non-floating point field", () => {
     collection({
       name: "test",
       fields: [
         {
-          type: "object",
+          type: "string",
           name: "field",
+          // @ts-expect-error This is erroring as expected
+          vec_dist: "cosine",
         },
       ],
-      // @ts-expect-error This is erroring as expected
-      enable_nested_fields: false,
     });
-  });
-  it("can have a nested object field with nested fields enabled", () => {
     collection({
+      name: "test",
+      fields: [
+        // @ts-expect-error This is erroring as expected
+        {
+          type: "string",
+          name: "field",
+          num_dim: 384,
+        },
+      ],
+    });
+    collection({
+      name: "test",
+      fields: [
+        // @ts-expect-error This is erroring as expected
+        {
+          type: "string",
+          name: "field",
+          hnsw_params: {
+            M: 16,
+            ef_construction: 200,
+          },
+        },
+      ],
+    });
+    //The request will go on through Typesense, but it will not add the parameters to the schema
+  });
+  it("can have a nested object field with nested fields enabled", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -107,18 +262,48 @@ describe("collection tests", () => {
       ],
       enable_nested_fields: true,
     });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "object",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "",
+      enable_nested_fields: true,
+      symbols_to_index: [],
+      token_separators: [],
+    });
   });
-  it("can't have a non-indexed-field facetted by", () => {
-    collection({
+  it("can't have a non-indexed-field facetted by", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         // @ts-expect-error This is erroring as expected
         { type: "string", name: "field", index: false, facet: true },
       ],
     });
+    await expect(create(schema, config)).rejects.toThrow(
+      "Field `field` cannot be a facet since it's marked as non-indexable.",
+    );
   });
-  it("can have a non-index field sorted by", () => {
-    collection({
+  it("can have a non-index field sorted by", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -129,9 +314,36 @@ describe("collection tests", () => {
         },
       ],
     });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "string",
+          facet: false,
+          index: false,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: true,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
+    });
   });
-  it("can have a facet set to true if the index is undefined or true", () => {
-    collection({
+  it("can have a facet set to true if the index is undefined or true", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -147,9 +359,48 @@ describe("collection tests", () => {
         },
       ],
     });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "string",
+          facet: true,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+        },
+        {
+          name: "field2",
+          type: "string",
+          facet: true,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
+    });
   });
-  it("can have a sort set to true if the index is undefined or true", () => {
-    collection({
+  it("can have a sort set to true if the index is undefined or true", async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -165,9 +416,48 @@ describe("collection tests", () => {
         },
       ],
     });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "string",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: true,
+          stem: false,
+          store: true,
+        },
+        {
+          name: "field2",
+          type: "string",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: true,
+          stem: false,
+          store: true,
+        },
+      ],
+      default_sorting_field: "",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
+    });
   });
-  it("can have an embedding field", () => {
-    collection({
+  it("can have an embedding field", { timeout: 30000 }, async () => {
+    const schema = collection({
       name: "test",
       fields: [
         {
@@ -179,13 +469,76 @@ describe("collection tests", () => {
           name: "field3",
           type: "float[]",
           embed: {
-            from: ["field2"],
+            from: ["field"],
             model_config: {
-              model_name: "test",
+              model_name: "ts/e5-small",
             },
           },
         },
       ],
+    });
+    const result = await create(schema, config);
+
+    const { created_at, ...expectedResult } = result;
+
+    expect(created_at).toBeCloseTo(Date.now() / 1000, -3);
+    expect(expectedResult).toStrictEqual({
+      name: "test",
+      num_documents: 0,
+      fields: [
+        {
+          name: "field",
+          type: "string",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+        },
+        {
+          name: "field2",
+          type: "string",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+        },
+        {
+          name: "field3",
+          type: "float[]",
+          facet: false,
+          index: true,
+          locale: "",
+          infix: false,
+          optional: false,
+          sort: false,
+          stem: false,
+          store: true,
+          embed: {
+            from: ["field"],
+            model_config: {
+              model_name: "ts/e5-small",
+            },
+          },
+          hnsw_params: {
+            M: 16,
+            ef_construction: 200,
+          },
+          vec_dist: "cosine",
+          num_dim: 384,
+        },
+      ],
+      default_sorting_field: "",
+      enable_nested_fields: false,
+      symbols_to_index: [],
+      token_separators: [],
     });
   });
   it("can't have an embedding field with a type other than float[]", () => {
@@ -196,18 +549,23 @@ describe("collection tests", () => {
           type: "string",
           name: "field",
         },
+        // @ts-expect-error This is erroring as expected
         {
           name: "field2",
-          type: "float[]",
+          type: "string[]",
           embed: {
             from: ["field"],
             model_config: {
-              model_name: "test",
+              model_name: "ts/e5-small",
             },
           },
         },
       ],
     });
+    // INFO: Non-float[] embeddings are not supported, but the schema is still created, this is a bug (fixed on upstream, but not on 27.1 )
+    // await expect(create(schema, config)).rejects.toThrow(
+    //   "Fields with the `embed` parameter can only be of type `float[]`.",
+    // );
   });
 });
 
