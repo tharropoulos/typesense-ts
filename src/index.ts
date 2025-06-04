7bb8e3add9de221f0442dc94fff1b2b85daba611
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-06-04 10:57:14 +0300

    feat(api): add overrides to entrypoint exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { multisearchEntry } from "./multisearch";
 export { collection, retrieveAllCollections } from "./http/fetch/collection";
 export { alias, retrieveAllAliases } from "./http/fetch/alias";
 export { validateCollectionUpdate } from "./collection";
+export { override, retrieveAllOverrides } from "./http/fetch/override";
 export {
   configure,
   setDefaultConfiguration,
@@ -13,3 +14,4 @@ export {
 export type * from "./config";
 export type { Collections } from "./collection";
 export type { Aliases } from "./alias";
+export type { Overrides } from "./override";
