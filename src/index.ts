796b5063fadd0ecb8e5fbbeff1f933db17fb3324
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-06-05 16:35:03 +0300

    feat(api): export analytic based functions on entrypoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ export { validateCollectionUpdate } from "./collection";
 export { override, retrieveAllOverrides } from "./http/fetch/override";
 export { stopword, retrieveAllStopwords } from "./http/fetch/stopword";
 export {
+  analyticsRule,
+  sendEvent,
+  retrieveAllAnalyticsRules,
+} from "./http/fetch/analytics";
+export {
   configure,
   setDefaultConfiguration,
   getDefaultConfiguration,
@@ -17,4 +22,5 @@ export type { Collections, InferNativeType } from "./collection";
 export type { Aliases } from "./alias";
 export type { Overrides } from "./override";
 export type { Stopwords } from "./stopword";
+export type { AnalyticsRules } from "./analytics/rules";
 export type { SearchResponse } from "./search";
